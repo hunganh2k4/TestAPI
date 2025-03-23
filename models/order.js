8ef320cc115b0aacd463fab50edd5baf3810e925
HungAnh <phanhunganh2004@gmail.com>
2025-03-23 17:52:34 +0700

    12345

--- models/order.js
+++ models/order.js
@@ -8,7 +8,6 @@ const orderSchema = new mongoose.Schema({
             productImage: { type: Number, required: true }, // Hình ảnh sản phẩm
             productName: { type: String, required: true }, // Tên sản phẩm
             quantity: { type: Number, required: true }, // Số lượng
-            price: { type: Number, required: true } // Giá của từng sản phẩm (không tổng)
         }
     ],
     userId: { type: String, required: true } // ID người dùng đặt hàng
